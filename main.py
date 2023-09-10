f0f33068e4abc4e0151fec7164635475314ae52f
ErnieLud <a20161018@pucp.edu.pe>
2023-09-10 15:29:40 -0500

    dev: adding activity data collection

--- main.py
+++ main.py
@@ -1,37 +1,58 @@
-from datetime import time
+import datetime
 
 
 def set_data_collection_type():
-    collect_type = input('Choose your data collection type (T = time of the day, C = counting hours)\n')
+    collect_type = input('Choose your data collection type (T = time of the day, C = counting hours & minutes)\n')
     while collect_type not in ['t', 'T', 'c', 'C']:
-        collect_type = input('That is not right, please try again (T = time of the day, C = counting hours)\n')
+        collect_type = input('That is not right, try again (T = time of the day, C = counting hours & minutes)\n')
     return 't' if collect_type in ['t', 'T'] else 'c'
 
 
-def collect_data_by_time():
-    time_sum = 0.0
-    wake_up_time = ask_for_time('At what time did you woke up? Write 1 or 2 numbers:\n'
-                                '1st: Hour (0-23)\n'
-                                '2nd: Minutes (0-59)\n')
-
-
 def collect_data_by_count():
+    time_sum, sleeping = 0.0, False
+    wake_up_time = ask_for_time('At what time did you woke up? '
+                                'Enter 1 or 2 numbers: hour (0-23) and minutes (0-59)\n')
+    print(f'So you woke up at {wake_up_time}')
+    time_sum += wake_up_time.hour + wake_up_time.minute / 60
+    while time_sum <= 24.0 and not sleeping:
+        activity_name, hours, minutes = get_activity_data('What did you do next & for how many hours and minutes? '
+                                                          'Enter 1 or 2 numbers: hours (0-23) and minutes (0-59)\n')
+        print(f'So you did {activity_name} for {str(hours)} hours and {str(minutes)} minutes')
+        time_sum += hours + minutes / 60
+        current_time = datetime.time(int(time_sum), int((time_sum - int(time_sum)) * 60))
+        print(f'It is {current_time} now.')
+
+
+def collect_data_by_time():
     pass
 
 
-def ask_for_time(msg, wrong_msg = 'That is not right, please try again'):
-    done, input_time = False, time()
+def get_activity_data(msg, wrong_msg='That is not right, please try again\n'):
+    activity_name, hours, minutes, done = '', 0, 0, False
+    while not done:
+        try:
+            activity_data = input(msg).split()
+            activity_name = activity_data[0]
+            hours = int(activity_data[1])
+            minutes = int(activity_data[2]) if len(activity_data) > 2 else 0
+            done = True
+        except (KeyError, ValueError):
+            print(wrong_msg)
+    return activity_name, hours, minutes
+
+
+def ask_for_time(msg, wrong_msg='That is not right, please try again\n'):
+    done, input_time = False, datetime.time()
     while not done:
         try:
             input_time = input(msg)
             numbers = input_time.split()
             hour = int(numbers[0])
             minutes = int(numbers[1]) if len(numbers) > 1 else 0
-            input_time = time(hour, minutes)
+            input_time = datetime.time(hour, minutes)
             done = True
         except (KeyError, ValueError):
             print(wrong_msg)
-    print(input_time)
     return input_time
 
 
