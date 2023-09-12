f143747fceea2ff5afceb0a4e93eb669e195e898
ErnieSumoso <144075326+ErnieSumoso@users.noreply.github.com>
2023-09-11 23:01:00 -0500

    dev: testing github commits

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def collect_data_by_count():
         time_sum += hours + minutes / 60
         current_time = datetime.time(int(time_sum), int((time_sum - int(time_sum)) * 60))
         print(f'It is {current_time} now.')
-        # checking commit on github 2
+        # checking commit on github 3
 
 
 def collect_data_by_time():
